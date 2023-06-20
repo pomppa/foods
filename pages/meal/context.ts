a772920eaa2982e696603f8bf713c14f23e7928b
Tuomas Änäkkälä <tuomas.anakkala@gmail.com>
2023-06-20 22:27:38 +0300

    remove legacy stuff

--- pages/meal/context.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-import { createContext } from 'react';
-
-export const Context = createContext({
-  value: '',
-  //   setValue: () => {},
-});
