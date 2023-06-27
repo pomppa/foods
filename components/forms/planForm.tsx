5c0cf9bc26f92bf4d57e2e2b9343ce530d8b2291
Tuomas Änäkkälä <tuomas.anakkala@gmail.com>
2023-06-27 20:49:30 +0300

    adding types for planform

--- components/forms/planForm.tsx
+++ components/forms/planForm.tsx
@@ -1,21 +1,28 @@
 import { useState } from 'react';
 import { Button, Box, Grid } from '@mui/material';
 import IngredientAutocomplete from './ingredientAutocomplete';
-import { FormValues } from '../../interfaces';
+import { FormValue, IngredientI } from '../../interfaces';
 import { Option } from '../../interfaces';
-import { Ingredient } from '@prisma/client';
+
+type Props = {
+  data: IngredientI[];
+  onChange: (formValues: FormValue[]) => void;
+  formValues?: FormValue[];
+};
+
 /**
  * Planner view, inputs for selecting ingredients and saving as a meal
  * @param props
  * @returns
  */
-export default function PlanForm(props) {
+export default function PlanForm(props: Props) {
   const { data, onChange } = props;
-  const options: Option[] = data.map((element: Ingredient) => {
+
+  const options: Option[] = data.map((element: IngredientI) => {
     return { label: element.name, id: element.id };
   });
 
-  const [ingredients, setIngredients] = useState<FormValues[]>(
+  const [ingredients, setIngredients] = useState<FormValue[]>(
     props.formValues || [{ ingredient: 0, weight: 0 }],
   );
 
@@ -27,22 +34,22 @@ export default function PlanForm(props) {
     setIngredients([...ingredients, { ingredient: 0, weight: 0 }]);
   };
 
-  const removeIngredient = (index) => {
-    const updatedIngredients = [...ingredients];
+  const removeIngredient = (index: number) => {
+    const updatedIngredients: FormValue[] = [...ingredients];
     updatedIngredients.splice(index, 1);
     setIngredients(updatedIngredients);
     onChange(updatedIngredients);
   };
 
-  const handleIngredientChange = (index, ingredient) => {
-    const updatedIngredients = [...ingredients];
+  const handleIngredientChange = (index: number, ingredient: number) => {
+    const updatedIngredients: FormValue[] = [...ingredients];
     updatedIngredients[index].ingredient = ingredient;
     setIngredients(updatedIngredients);
     onChange(updatedIngredients);
   };
 
-  const handleWeightChange = (index, weight) => {
-    const updatedIngredients = [...ingredients];
+  const handleWeightChange = (index: number, weight: number) => {
+    const updatedIngredients: FormValue[] = [...ingredients];
     updatedIngredients[index].weight = weight;
     setIngredients(updatedIngredients);
     onChange(updatedIngredients);
@@ -58,10 +65,12 @@ export default function PlanForm(props) {
               value={ingredient.ingredient}
               weight={ingredient.weight}
               options={options}
-              onIngredientChange={(updatedIngredient) =>
+              onIngredientChange={(updatedIngredient: number) =>
                 handleIngredientChange(index, updatedIngredient)
               }
-              onWeightChange={(weight) => handleWeightChange(index, weight)}
+              onWeightChange={(weight: number) =>
+                handleWeightChange(index, weight)
+              }
               disabledOptions={disabledOptions}
             />
           ))}
