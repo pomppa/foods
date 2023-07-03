809fda244ba94883b5c3bcd18e80a5909acef8a2
Tuomas Änäkkälä <tuomas.anakkala@gmail.com>
2023-07-04 00:01:11 +0300

    meals

--- pages/meals.tsx
+++ pages/meals.tsx
@@ -2,18 +2,13 @@ import { Meal } from '@prisma/client';
 import { getAllMeals } from './api/meals';
 import { useRouter } from 'next/router';
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Box,
   Grid,
-  IconButton,
   List,
   ListItem,
+  ListItemIcon,
   ListItemText,
 } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
 
 type Props = {
   meals: Omit<Meal, 'created_at' | 'updated_at'>[];
@@ -32,7 +27,6 @@ export default function Meals(props: Props) {
   const handleEditClick = (id: number) => {
     router.push(`/meals/${id}`);
   };
-
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={6}>
@@ -40,38 +34,17 @@ export default function Meals(props: Props) {
         <small>Open a meal to edit</small>
         <List>
           {data.map((x) => (
-            <Accordion key={x.id}>
-              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                <ListItemText primary={x.name} />
-              </AccordionSummary>
-              <AccordionDetails>
-                <List>
-                  {/* prisma-json-types-generator */}
-                  {x.formValues.map((value, index) => (
-                    <ListItem key={index}>
-                      <ListItemText
-                        primary={`${value.ingredient_id}`}
-                        secondary={`Weight: ${value.weight}`}
-                      />
-                    </ListItem>
-                  ))}
-                </List>
-                <Box
-                  display="flex"
-                  justifyContent="flex-end"
-                  width="100%"
-                  marginTop="auto"
-                >
-                  <IconButton
-                    aria-label="Edit"
-                    color="primary"
-                    onClick={() => handleEditClick(x.id)}
-                  >
-                    <EditIcon />
-                  </IconButton>
-                </Box>
-              </AccordionDetails>
-            </Accordion>
+            <ListItem
+              key={x.id}
+              button
+              component="a"
+              href={`/meals/${x.id}/edit`}
+            >
+              <ListItemText primary={x.name} />
+              <ListItemIcon>
+                <ArrowCircleRightIcon />
+              </ListItemIcon>
+            </ListItem>
           ))}
         </List>
       </Grid>
