9b8a99e471fff3918468499ce77e8bb8068e4ae6
Tuomas Änäkkälä <tuomas.anakkala@gmail.com>
2023-07-17 15:09:25 +0300

    install & configure next-pwa

--- components/design/layout.tsx
+++ components/design/layout.tsx
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <title>Foods</title>
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <PersistentDrawerLeft />
       <Box sx={{ ml: 3, mr: 3, mb: 3, maxWidth: 'md' }}>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  mode: 'production',
+  skipWaiting: true,
+  include: ['production'],
+});
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = nextConfig;
+});
