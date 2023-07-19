4d3e017776b2deae0d944abd0707d0224b527e27
Tuomas Änäkkälä <tuomas.anakkala@gmail.com>
2023-07-19 13:01:56 +0300

    remove backups

--- migrations/20230702202347_form_values/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `meal` ADD COLUMN `formValues` JSON NULL;
--- migrations/20230703125804_updatedat/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `fineli_ingredient` ALTER COLUMN `updated_at` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `ingredient` ALTER COLUMN `updated_at` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `meal` ALTER COLUMN `updated_at` DROP DEFAULT;
