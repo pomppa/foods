f7d01f661ac9772167763397b9a215687f542bec
Tuomas Änäkkälä <tuomas.anakkala@gmail.com>
2023-07-19 17:35:59 +0300

    tsconfig include paths for types

---
+++ types/next-auth.d.ts
@@ -0,0 +1,12 @@
+import type { ISODateString } from 'next-auth';
+declare module 'next-auth' {
+  interface Session {
+    user?: {
+      id?: string | null;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+    };
+    expires: ISODateString;
+  }
+}
